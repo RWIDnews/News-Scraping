c49e4cf3ce6438837cada8b88b8ab34059a451ea
kevinsultana <kevinpdg.ks@gmail.com>
2024-10-02 16:23:15 +0700

    coba lagi

---
+++ cobafilebarulagi.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function cobafilebarulagi() {
+  return (
+    <View>
+      <Text>cobafilebarulagi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
