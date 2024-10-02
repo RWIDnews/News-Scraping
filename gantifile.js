8e3003d5cd90c23f68334575311f1548469a4715
kevinsultana <kevinpdg.ks@gmail.com>
2024-10-02 16:18:31 +0700

    testing kevin commit

--- gantifile.js
+++ gantifile.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-export default function test() {
+export default function gantifile() {
   return (
     <View>
-      <Text>test</Text>
+      <Text>gantifile</Text>
     </View>
   );
 }
